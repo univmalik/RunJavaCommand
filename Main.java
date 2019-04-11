d68a335278bd4d11b42aa982f54ac5f0d62da591
ADEEL MALIK <43076996+univmalik@users.noreply.github.com>
2019-04-12 01:55:47 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String args[]) {
-
+        //Sample below to run "java -version"
         RunCmd rcd = new RunCmd();
         System.out.println(rcd.JavaRunTime("java -version"));
     }
